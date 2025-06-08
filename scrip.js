620a5b07bb68dde28b04b7311c76041b2bd9fb3b
williamC9455 <ca.williamg@gmail.com>
2025-06-08 19:46:16 +0200

    Update scrip.js

---
+++ scrip.js
@@ -0,0 +1,27 @@
+body {
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+header, main, footer {
+  width: 80%;
+  max-width: 900px;
+  margin: 20px auto;
+  text-align: center;
+}
+
+nav ul {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+}
+
+section {
+  margin: 40px 0;
+}
